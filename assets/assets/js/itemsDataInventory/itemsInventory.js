05b9062435ea325f98fe076dafd8d3dc09be287e
alfarizki19 <alfarizki.19.ar@gmail.com>
2025-08-13 22:30:37 +0700

    Upload JS Files

---
+++ assets/assets/js/itemsDataInventory/itemsInventory.js
@@ -0,0 +1,20 @@
+const partBarel = {
+    "barel00100101":
+    {
+        itemsID : "barel00100101",
+        shopifyID : "",
+        namaPart : "",
+        harga : 0,
+        stock :0,
+        quantity : 0,
+    },
+    "barel00200101":
+    {
+        itemsID : "barel00100101",
+        shopifyID : "",
+        namaPart : "",
+        harga : 0,
+        stock :0,
+        quantity : 0,
+    }
+}
